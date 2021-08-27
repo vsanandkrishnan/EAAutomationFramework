30832d89cefd426285db8dd1417c9a634a963e92
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-08-27 08:28:40 +0530

    Added Enum Folder in helpers
    Added Enum Folder in helpers

---
+++ EATestProject/Helpers/Enums/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EAAutoFramework.Helpers.Enums
+{
+    class Employee
+    {
+        public enum EmployeeNames
+        {
+            Anand,
+            Nithin,
+            Vyshak
+        }
+    }
+}
